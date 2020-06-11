5a49c57a06b4517fad5c61e1c762133ca2dd7ddf
khanr <khanriza@gmail.com>
2020-06-11 01:49:36 -0400

    established connection with pg and generated schema file

---
+++ backend/db/schema.rb
@@ -0,0 +1,22 @@
+ActiveRecord::Schema.define(version: 2020_06_11_054148) do
+
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "blogs", "users"
+end
