268a14107a0fe71e00b24d38e0bf8f22b6603a73
khanr <khanriza@gmail.com>
2020-06-13 17:57:06 -0400

    added login form

--- frontend/components/SignIn.vue
+++ frontend/components/SignIn.vue
@@ -1,16 +1,29 @@
-Pavle Ciric 6 months ago updated Popup.vue component:
 <template>
   <div class="text-center">
     <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on }">
-        <v-btn class="success" dark v-on="on">
-          Sign In
-        </v-btn>
+        <v-btn class="success" dark v-on="on">Sign In</v-btn>
       </template>
       <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title
+        <v-card-title class="headline dark lighten-2" primary-title
           >Add a New Project</v-card-title
         >
+        <v-card-actions>
+          <v-form ref="form" lazy-validation @submit.prevent="signIn">
+            <v-text-field
+              v-model="email"
+              label="E-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              type="password"
+              label="Password"
+              required
+            ></v-text-field>
+            <input type="submit" value="Sign In" />
+          </v-form>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
@@ -19,7 +32,17 @@ Pavle Ciric 6 months ago updated Popup.vue component:
 <script>
 export default {
   data: () => ({
-    dialog: false
-  })
+    dialog: false,
+    email: '',
+    password: ''
+  }),
+  methods: {
+    signIn() {
+      this.$store.dispatch('user/signIn', {
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
 }
 </script>
