a43e1b1296ac084451c1313ff4b8b190f122bd6c
khanr <khanriza@gmail.com>
2020-06-15 00:09:39 -0400

    adjusted portfolio controller for edit and showing

--- backend/app/controllers/portfolios_controller.rb
+++ backend/app/controllers/portfolios_controller.rb
@@ -1,29 +1,23 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:edit, :update, :create, :destroy]
 
-  # GET /portfolios
-  # GET /portfolios.json
   def index
     @portfolios = Portfolio.all
     render json: @portfolios
   end
 
-  # GET /portfolios/1
-  # GET /portfolios/1.json
   def show
+    render json: @portfolio
   end
 
-  # GET /portfolios/new
   def new
     @portfolio = Portfolio.new
   end
 
-  # GET /portfolios/1/edit
   def edit
   end
 
-  # POST /portfolios
-  # POST /portfolios.json
   def create
     @portfolio = Portfolio.new(portfolio_params)
 
@@ -38,8 +32,6 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /portfolios/1
-  # PATCH/PUT /portfolios/1.json
   def update
     respond_to do |format|
       if @portfolio.update(portfolio_params)
@@ -52,8 +44,6 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # DELETE /portfolios/1
-  # DELETE /portfolios/1.json
   def destroy
     @portfolio.destroy
     respond_to do |format|
@@ -63,12 +53,10 @@ class PortfoliosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
       @portfolio = Portfolio.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :imagepreview, :imagemainone, :imagemaintwo, :imagemainthree, :user_id)
     end
